38534d52a6bb1c377e76b901ee4bf40e7b62202e
RamblinRoyce <ChingRoyce@gmail.com>
2016-07-09 15:31:15 -0500

    rching finish game.js

--- development/game.js
+++ development/game.js
@@ -1,4 +1,10 @@
-exports.game = {
-	//this.wordChoices = ["heart and soul", "relax", "burning up", "manic monday", "into the groove", "invisible touch", "rebel yell"];
-	wordBank : ["relax"]
-};
+// exports.game = {
+// 	//this.wordChoices = ["heart and soul", "relax", "burning up", "manic monday", "into the groove", "invisible touch", "rebel yell"];
+// 	wordBank : ["relax"]
+// };
+
+function WordBank() {
+	this.wordBank = ['scooby-doo', 'captain caveman', 'snorks', 'smurfs', 'pinky and the brain', 'ducktales', 'thundercats', 'the flintstones', 'the jetsons', 'transformers', 'looney tunes', 'ducktales', 'gi joe'];
+}
+
+module.exports = WordBank;
